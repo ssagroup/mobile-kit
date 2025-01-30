e0fc4125bd3e1a46039f74e24736fc767e6275a4
Oksana Liahusha <oksana.liahusha@ssa.group>
2025-01-30 22:21:47 +0700

    Fixed color and icon for splash screen

--- mobile_kit/lib/src/core/resources/colors.dart
+++ mobile_kit/lib/src/core/resources/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorPalette {
   // static const bluishAction = Color(0xff4178E1);
-  static const greyBackground = Color(0xffD9D9D9);
+  static const greyBackground = Color(0xffEEF1F7);
   // static const pinkBackground = Color(0xfff0816b);
   // static const greenBackground = Color(0xff52c587);
   static const grayText = Color(0xff2B2D31);
