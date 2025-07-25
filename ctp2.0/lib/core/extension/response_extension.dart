696d94f44e949ad11c49a449164ff9f6dca731bc
Oksana Liahusha <oksana.liahusha@ssa.group>
2025-07-25 19:43:42 +0200

    Replace ctp-prototype and ctp2.0 to different repo

--- ctp2.0/lib/core/extension/response_extension.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dio/dio.dart';
-
-extension ResponseExtension on Response {
-  bool get isSuccessful => statusCode! >= 200 && statusCode! < 300;
-}
--- ssa_mobile_kit_demo/android/app/src/main/kotlin/com/ssa/mobile/kit/ssa_mobile_kit_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ssa.mobile.kit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
